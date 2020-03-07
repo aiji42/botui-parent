dcea341faf78622beaaf458dbe1d0299c1b53966
Aiji Uejima <aiji42@gmail.com>
2020-03-07 22:25:35 +0900

    Fix warning process.env.COMMIT_REF

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ export default {
       BOTUI_CHILD_ENDPOINT: process.env.BOTUI_CHILD_ENDPOINT,
       BOTUI_ACTIVATE_RATE: process.env.BOTUI_ACTIVATE_RATE,
       BUGSNAG_API_KEY: process.env.BUGSNAG_API_KEY,
-      COMMIT_REF: process.env.COMMIT_REF
+      COMMIT_REF: process.env.COMMIT_REF || 'xxxxx'
     })
   ]
 }
