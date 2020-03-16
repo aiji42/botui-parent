cb19003e78db4f1fd845849f1729a8d7f001b8a1
Aiji Uejima <aiji42@gmail.com>
2020-03-16 22:09:26 +0900

    開発環境では強制的に立ち上げる

--- src/settings/launchCondition.js
+++ src/settings/launchCondition.js
@@ -1,5 +1,6 @@
 const domainIsTrue = document.location.host === 'nelture.com';
 const pathIsTrue = document.location.pathname === '/shopping/';
 const referrerIsTrue = document.referrer.includes('/cart/');
+const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const launchCondition = domainIsTrue && pathIsTrue && referrerIsTrue;
+export const launchCondition = isDevelopment || domainIsTrue && pathIsTrue && referrerIsTrue;
