93dc39325b95a7189a20898d3c1ffe598eb13301
Aiji Uejima <aiji42@gmail.com>
2020-04-12 11:07:38 +0900

    確認フォームのデザイン

--- src/apis/confirm.js
+++ src/apis/confirm.js
@@ -39,31 +39,38 @@ export const confirmHTML = async (data) => {
   return `
     <h3>ご注文内容</h3>
     ${items.map(item => `
-      <img src="${item.image}">
-      <p>${item.name}</p>
-      ${item.description.length > 0 ? `<p>${item.description}</p>` : ''}
-      <p>${item.price}円(税込)</p>
-      <p>数量 ${item.quantity}</p>
-      <p>小計 ${item.totalPrice}円</p>
+      <div style="margin-bottom:5px">
+        <img src="${item.image}" style="width:40%;display:inline-block;" >
+        <div style="width:57%;display:inline-block;">
+          <p style="margin:0px">${item.name}</p>
+          ${item.description.length > 0 ? `<p  style="margin:0px">${item.description}</p>` : ''}
+          <p style="margin:0px">${item.price}円(税込)</p>
+          <p style="margin:0px">数量 ${item.quantity}</p>
+          <p style="margin:0px">小計 ${item.totalPrice}円</p>
+        </div>
+      </div>
     `).join('')}
     <h3>お支払い金額</h3>
-    <p>商品合計 ${total.itemSum}円</p>
-    <p>送料 ${total.postage}円</p>
-    ${total.hasPaymentCharge ? `<p>支払手数料 ${total.paymentCharge}円</p>` : ''}
-    ${total.hasWrappingCharge ? `<p>ラッピング料 ${total.wrappingCharge}円</p>` : ''}
-    ${total.hasProductCoupon ? `<p>クーポン値引き -${total.productCoupon}円</p>` : ''}
-    ${total.hasPostageCoupon ? `<p>クーポン送料値引き -${total.postageCoupon}円</p>` : ''}
-    ${total.hasPointDiscount ? `<p>ポイント利用 -${total.pointDiscount}円</p>` : ''}
-    <p>総合計 ${total.totalPrice}円</p>
+    <dl>
+      <dt style="float:left;">商品合計</dt><dd style="margin-left:100px;">${total.itemSum}円</dd>
+      <dt style="float:left;">送料</dt><dd style="margin-left:100px;">${total.postage}円</dd>
+      ${total.hasPaymentCharge ? `<dt style="float:left;">支払手数料</dt><dd style="margin-left:100px;">${total.paymentCharge}円</dd>` : ''}
+      ${total.hasWrappingCharge ? `<dt style="float:left;">ラッピング料</dt><dd style="margin-left:100px;">${total.wrappingCharge}円</dd>` : ''}
+      ${total.hasProductCoupon ? `<dt style="float:left;">クーポン値引き</dt><dd style="margin-left:100px;">-${total.productCoupon}円</dd>` : ''}
+      ${total.hasPostageCoupon ? `<dt style="float:left;">クーポン送料値引き</dt><dd style="margin-left:100px;">-${total.postageCoupon}円</dd>` : ''}
+      ${total.hasPointDiscount ? `<dt style="float:left;">ポイント利用</dt><dd style="margin-left:100px;">-${total.pointDiscount}円</dd>` : ''}
+      <dt style="float:left;">総合計</dt><dd style="margin-left:100px;">${total.totalPrice}円</dd>
+    </dl>
+
     <h3>お届け先</h3>
-    <p>${address.lastName} ${address.firstName}</p>
-    <p>〒${address.zipCode}</p>
-    <p>${address.prefecture}${address.addressLine1}${address.addressLine2}${address.addressLine3}</p>
+    <p style="margin:0px">${address.lastName} ${address.firstName}</p>
+    <p style="margin:0px">〒${address.zipCode}</p>
+    <p style="margin:0px">${address.prefecture}${address.addressLine1}${address.addressLine2}${address.addressLine3}</p>
     <h3>お支払い方法</h3>
-    <p>${paymentMethod}</p>
+    <p style="margin:0px">${paymentMethod}</p>
     ${coupons.length > 0 ? `
       <h3>ご利用のクーポン</h3>
-      ${coupons.map(coupon => `<p>${coupon}</p>`).join('')}
+      ${coupons.map(coupon => `<p style="margin:0px">${coupon}</p>`).join('')}
     ` : ''}
   `;
 };
