7b87aa4403e329a79b794fedc759f26146269259
Aiji Uejima <aiji42@gmail.com>
2020-06-07 19:15:57 +0900

    立ち上げ条件を複数のpathnameにも対応,デバイスタイプによる立ち上げ条件の対応

--- src/settings/launchCondition.js
+++ src/settings/launchCondition.js
@@ -1,8 +1,46 @@
+import DeviceDetector from 'device-detector-js';
+
 const conditions = JSON.parse(process.env.BOTUI_LAUNCH_CONDITION || '{}');
 
+const hostCheck = (conditions) => {
+  const checker = (condition) => document.location.host.includes(condition);
+  if (conditions instanceof Array) return conditions.some(checker);
+  else return checker(conditions);
+};
+
+const pathnameCheck = (conditions) => {
+  const checker = (condition) => document.location.pathname.includes(condition);
+  if (conditions instanceof Array) return conditions.some(checker);
+  else return checker(conditions);
+};
+
+const searchCheck = (conditions) => {
+  const checker = (condition) => document.location.search.includes(condition);
+  if (conditions instanceof Array) return conditions.some(checker);
+  else return checker(conditions);
+};
+
+const referrerCheck = (conditions) => {
+  const checker = (condition) => document.referrer.includes(condition);
+  if (conditions instanceof Array) return conditions.some(checker);
+  else return checker(conditions);
+};
+
+const deviceCheck = (conditions) => {
+  const deviceDetector = new DeviceDetector();
+  const { device } = deviceDetector.parse(window.navigator.userAgent);
+  if (!device) return false;
+
+  const checker = (condition) => device.type === condition;
+
+  if (conditions instanceof Array) return conditions.some(checker);
+  else return checker(conditions);
+};
+
 export const launchCondition = Object.keys(conditions).every(key => {
-  if (key === 'host') return document.location.host.includes(conditions[key]);
-  if (key === 'pathname') return document.location.pathname.includes(conditions[key]);
-  if (key === 'search') return document.location.search.includes(conditions[key]);
-  if (key === 'referrer') return document.referrer.includes(conditions[key]);
+  if (key === 'host') return hostCheck(conditions[key]);
+  if (key === 'pathname') return pathnameCheck(conditions[key]);
+  if (key === 'search') return searchCheck(conditions[key]);
+  if (key === 'referrer') return referrerCheck(conditions[key]);
+  if (key === 'device') return deviceCheck(conditions[key]);
 });
