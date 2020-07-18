143d2c20a09e4b197f7763d49948d551c88cbcb3
Aiji Uejima <aiji42@gmail.com>
2020-07-18 15:34:39 +0900

    クローズボタン

---
+++ src/components/Modal/Close/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+
+const close = css`
+  width: 25px;
+  height: 25px;
+  :before, :after {
+    position: absolute;
+    left: 15px;
+    content: ' ';
+    height: 33px;
+    width: 2px;
+    background-color: #fff;
+  }
+  :before {
+    transform: rotate(45deg);
+  }
+  :after {
+    transform: rotate(-45deg);
+  }
+`;
+
+const Close = ({ onClick }) => {
+  return <div onClick={onClick} css={close} />;
+};
+
+Close.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+export default Close;
